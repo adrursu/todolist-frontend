0c390577e5bdaed30edb5039f89f6628ba736e75
adrursu <adrianursu@gmail.com>
2024-10-09 13:43:23 -0400

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-const API_URL = 'todolistbckendcontainer-e4a6bsb8gyf2a9a7.canadacentral-01.azurewebsites.net:3000/api/todos';
+const API_URL = 'todolistbckendcontainer-e4a6bsb8gyf2a9a7.canadacentral-01.azurewebsites.net/api/todos';
 
 window.onload = fetchTodos;
 
